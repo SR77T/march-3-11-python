9b3deace33730b9e9616fd6b94dbf29f94a50cfb
SR77T <sarthakrasaily@gmail.com>
2024-03-11 17:12:50 +0545

    Project

---
+++ day3/4_strings.py
@@ -0,0 +1,12 @@
+data = "Hello world. I am learning python"
+print(data[3: 10])
+print(data[8:2])
+print(data[:9])
+print(data[7:])
+
+print(data[-10: -2])
+print(data[-1: -9])
+print(data[2: -3])
+print(data[-3:])
+print(data[-4:])
+
