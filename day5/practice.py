230b66831ae27e9ce13c28b2e8d5315ef9f0f14a
SR77T <sarthakrasaily@gmail.com>
2024-03-14 18:16:56 +0545

    dictionary

---
+++ day5/practice.py
@@ -0,0 +1,16 @@
+employee = {
+    'name': 'sarthak',
+    'phone_no': 98505541,
+    'salary' : 2505080550,
+}
+print(employee['salary'])
+employee['phone_no'] = 98552558
+print(employee)
+employee['name'] = 'Gagan'
+print(employee)
+print(employee.keys())
+print(len(employee))
+print(employee.keys())
+print(employee['salary'])
+employee['salary'] = 3568952525
+print(employee)
